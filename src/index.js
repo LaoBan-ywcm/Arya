4a1c14352641a2c28a982df7591b2aa71d2dc9ee
LaoBan-ywcm <qiuqiandfqr@gmail.com>
2022-06-28 16:06:19 +0800

    :sparkles: add ant design

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { Layout } from 'antd';
+const { Header, Footer, Content } = Layout;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <div>init</div>
+  <div>
+    <Layout>
+      <Header>Header</Header>
+      <Content>Content</Content>
+      <Footer>Footer</Footer>
+    </Layout>
+  </div>
 );
 
 
